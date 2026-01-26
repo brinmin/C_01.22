6a54b3268733b37e21e7848a3f4529fa0d24dd0c
brinmin <brianmin1127@gmail.com>
2026-01-26 12:56:32 +0900

    while문

---
+++ project08/project08.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#define COUNT	5		// 메크로 상수
+int main()
+{
+	/*
+	
+	*****
+	*****
+	*****
+	*****
+	*****
+	
+	*/
+	const int NUMBE = 5;	// 상수: 값을 변경할 수 없음
+
+
+
+	// printf("*****"); (X)
+	//printf("*");(O)
+
+	int i = 0;
+	while (i < COUNT)
+	{
+		int j = 0;
+		while (j < COUNT)
+		{
+			printf("*");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+
+
+
+	return 0;
+
+}
+
+
